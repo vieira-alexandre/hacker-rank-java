7b6791deebb1e440f47434bad9513d985d233493
vieira-alexandre <alexandrer0x@hotmail.com>
2024-05-29 18:16:56 -0300

    10 - static initializer

---
+++ src/main/java/dev/alexandrevieira/basic/c10_static_initializer/Solution.java
@@ -0,0 +1,39 @@
+package dev.alexandrevieira.basic.c10_static_initializer;
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    private static final BufferedReader READER;
+    private static final int B;
+    private static final int H;
+    private static final boolean flag;
+
+    static {
+        READER = new BufferedReader(new InputStreamReader(System.in));
+
+        try {
+            B = Integer.parseInt(READER.readLine());
+            H = Integer.parseInt(READER.readLine());
+            flag = B > 0 && H > 0;
+
+            if (!flag) System.out.println("java.lang.Exception: Breadth and height must be positive");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void main(String[] args) {
+        if (flag) {
+            int area = B * H;
+            System.out.print(area);
+        }
+
+    }//end of main
+
+}//end of class
+
