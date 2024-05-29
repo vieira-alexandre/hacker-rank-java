cabd30fa9ce428d0b6414d26f46e2e538a2f38ca
vieira-alexandre <alexandrer0x@hotmail.com>
2024-05-29 18:49:10 -0300

    strings - valid username checker

---
+++ src/main/java/dev/alexandrevieira/strings/c11_valid_username_checker/UsernameValidator.java
@@ -0,0 +1,24 @@
+package dev.alexandrevieira.strings.c11_valid_username_checker;
+
+import java.util.Scanner;
+
+class UsernameValidator {
+    public static final String regularExpression = "^[A-Za-z][A-Za-z0-9_]{7,29}$";
+}
+
+class Solution {
+    private static final Scanner scan = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int n = Integer.parseInt(scan.nextLine());
+        while (n-- != 0) {
+            String userName = scan.nextLine();
+
+            if (userName.matches(UsernameValidator.regularExpression)) {
+                System.out.println("Valid");
+            } else {
+                System.out.println("Invalid");
+            }
+        }
+    }
+}
