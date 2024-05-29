090a5969eb24b01e82026a164a53c9042d539155
vieira-alexandre <alexandrer0x@hotmail.com>
2024-05-29 18:08:39 -0300

    06 - loops 1

---
+++ src/main/java/dev/alexandrevieira/basic/c06_loops_1/Solution.java
@@ -0,0 +1,29 @@
+package dev.alexandrevieira.basic.c06_loops_1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+        bufferedReader.close();
+
+        StringBuilder sb = new StringBuilder();
+
+
+        for (int i = 1; i <= 10; i++) {
+            sb.append(n);
+            sb.append(" x ");
+            sb.append(i);
+            sb.append(" = ");
+            sb.append(n * i);
+            sb.append("\n");
+        }
+
+        System.out.println(sb);
+    }
+}
+
