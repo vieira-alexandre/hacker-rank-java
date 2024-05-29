1a7d76022bbf6a5bbeb418506a745a62e4f0b797
vieira-alexandre <alexandrer0x@hotmail.com>
2024-05-29 18:39:07 -0300

    strings - tokens

---
+++ src/main/java/dev/alexandrevieira/strings/c06_string_tokens/Solution.java
@@ -0,0 +1,28 @@
+package dev.alexandrevieira.strings.c06_string_tokens;
+
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String s = scan.nextLine().trim();
+
+        String regex = "[ !,?._'@]+";
+
+        String[] tokens = s.split(regex);
+
+        if (tokens.length == 1 && tokens[0].isEmpty()) {
+            System.out.println(0);
+            return;
+        }
+
+        System.out.println(tokens.length);
+
+        for (String token : tokens) {
+            System.out.println(token);
+        }
+
+        scan.close();
+    }
+}
