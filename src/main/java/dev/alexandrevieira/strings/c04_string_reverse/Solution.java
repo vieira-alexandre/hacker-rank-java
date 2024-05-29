d81371937f5e88f9734d4a4178499a26eeaa45f0
vieira-alexandre <alexandrer0x@hotmail.com>
2024-05-29 18:33:49 -0300

    strings - reverse

---
+++ src/main/java/dev/alexandrevieira/strings/c04_string_reverse/Solution.java
@@ -0,0 +1,25 @@
+package dev.alexandrevieira.strings.c04_string_reverse;
+
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String A = sc.next();
+
+        boolean equals = true;
+
+        for (int i = 0, k = A.length() - 1; i < A.length(); i++, k--) {
+            if (A.charAt(i) != A.charAt(k)) {
+                equals = false;
+                break;
+            }
+        }
+
+        System.out.println(equals ? "Yes" : "No");
+    }
+}
+
+
+
