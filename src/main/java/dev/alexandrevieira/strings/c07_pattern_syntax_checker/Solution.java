6d44975088fa8a9f2c4eaf64abf6c87840b293da
vieira-alexandre <alexandrer0x@hotmail.com>
2024-05-29 18:42:58 -0300

    strings - pattern syntax checker

---
+++ src/main/java/dev/alexandrevieira/strings/c07_pattern_syntax_checker/Solution.java
@@ -0,0 +1,28 @@
+package dev.alexandrevieira.strings.c07_pattern_syntax_checker;
+
+import java.util.Scanner;
+import java.util.regex.Pattern;
+import java.util.regex.PatternSyntaxException;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = Integer.parseInt(scan.nextLine());
+
+        for (int i = 0; i < n; i++) {
+            String string = scan.nextLine().trim();
+
+            try {
+                Pattern.compile(string);
+                System.out.println("Valid");
+            } catch (PatternSyntaxException ignored) {
+                System.out.println("Invalid");
+            }
+        }
+
+        scan.close();
+    }
+}
+
+
+
