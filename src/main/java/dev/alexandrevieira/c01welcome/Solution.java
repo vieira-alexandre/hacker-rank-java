282c60502a6dd272505325e17f579e9ea36e2786
vieira-alexandre <alexandrer0x@hotmail.com>
2024-05-29 17:56:45 -0300

    01 - welcome

---
+++ src/main/java/dev/alexandrevieira/c01welcome/Solution.java
@@ -0,0 +1,8 @@
+package dev.alexandrevieira.c01welcome;
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println("Hello, World.");
+        System.out.println("Hello, Java.");
+    }
+}
