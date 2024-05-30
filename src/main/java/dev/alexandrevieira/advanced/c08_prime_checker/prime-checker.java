69c1acf2023009df2e8c26418d52ea9a02864ebe
vieira-alexandre <alexandrer0x@hotmail.com>
2024-05-30 01:46:01 -0300

    advanced - prime checker

---
+++ src/main/java/dev/alexandrevieira/advanced/c08_prime_checker/prime-checker.java
@@ -0,0 +1,66 @@
+package dev.alexandrevieira.advanced.c08_prime_checker;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.lang.reflect.Method;
+import java.math.BigInteger;
+import java.util.HashSet;
+import java.util.Set;
+
+import static java.lang.System.in;
+
+class Prime {
+    void checkPrime(int... numbers) {
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < numbers.length; i++) {
+            if (isPrime(numbers[i])) {
+                sb.append(numbers[i]);
+                sb.append(" ");
+            }
+        }
+
+        System.out.println(sb);
+    }
+
+    private boolean isPrime(int n) {
+        return new BigInteger(String.valueOf(n)).isProbablePrime(1);
+    }
+}
+
+class Solution {
+
+    public static void main(String[] args) {
+        try {
+            BufferedReader br = new BufferedReader(new InputStreamReader(in));
+            int n1 = Integer.parseInt(br.readLine());
+            int n2 = Integer.parseInt(br.readLine());
+            int n3 = Integer.parseInt(br.readLine());
+            int n4 = Integer.parseInt(br.readLine());
+            int n5 = Integer.parseInt(br.readLine());
+            Prime ob = new Prime();
+            ob.checkPrime(n1);
+            ob.checkPrime(n1, n2);
+            ob.checkPrime(n1, n2, n3);
+            ob.checkPrime(n1, n2, n3, n4, n5);
+            Method[] methods = Prime.class.getDeclaredMethods();
+            Set<String> set = new HashSet<>();
+            boolean overload = false;
+            for (int i = 0; i < methods.length; i++) {
+                if (set.contains(methods[i].getName())) {
+                    overload = true;
+                    break;
+                }
+                set.add(methods[i].getName());
+
+            }
+            if (overload) {
+                throw new Exception("Overloading not allowed");
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
+}
+
