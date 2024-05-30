3afb35c77ab56e6a19998bd2d243e208d21cb818
vieira-alexandre <alexandrer0x@hotmail.com>
2024-05-29 23:40:29 -0300

    advanced - simple addition varargs

---
+++ src/main/java/dev/alexandrevieira/advanced/c01_simples_addition_varargs/simple-addition-varargs.java
@@ -0,0 +1,71 @@
+package dev.alexandrevieira.advanced.c01_simples_addition_varargs;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.lang.reflect.Method;
+import java.util.HashSet;
+import java.util.Set;
+
+
+class Add {
+    void add(int... numbers) {
+        StringBuilder sb = new StringBuilder();
+        int sum = 0;
+
+        for (int i = 0; i < numbers.length; i++) {
+            int value = numbers[i];
+            sb.append(value);
+            sum += value;
+
+            if (i != numbers.length - 1) {
+                sb.append("+");
+            }
+        }
+
+        sb.append("=");
+        sb.append(sum);
+
+        System.out.println(sb);
+    }
+
+}
+
+
+class Solution {
+
+    public static void main(String[] args) {
+        try {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            int n1 = Integer.parseInt(br.readLine());
+            int n2 = Integer.parseInt(br.readLine());
+            int n3 = Integer.parseInt(br.readLine());
+            int n4 = Integer.parseInt(br.readLine());
+            int n5 = Integer.parseInt(br.readLine());
+            int n6 = Integer.parseInt(br.readLine());
+            Add ob = new Add();
+            ob.add(n1, n2);
+            ob.add(n1, n2, n3);
+            ob.add(n1, n2, n3, n4, n5);
+            ob.add(n1, n2, n3, n4, n5, n6);
+            Method[] methods = Add.class.getDeclaredMethods();
+            Set<String> set = new HashSet<>();
+            boolean overload = false;
+            for (int i = 0; i < methods.length; i++) {
+                if (set.contains(methods[i].getName())) {
+                    overload = true;
+                    break;
+                }
+                set.add(methods[i].getName());
+
+            }
+            if (overload) {
+                throw new Exception("Overloading not allowed");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+}
+
